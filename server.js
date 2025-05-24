6c7d4c0eef6872f77fefef284742dbe960adf10c
Kurosepat <yasuhisa.kurose@gmail.com>
2025-05-24 14:14:50 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
+
 const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// 静的ファイル（HTMLやJS）を置いてるフォルダを指定
+// 静的ファイル（html, js, css）を公開
 app.use(express.static(path.join(__dirname)));
 
-// ルートパスにアクセスがあったときに upload.html を返す
+// 明示的に各ページを返すようにする
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/upload.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'upload.html'));
 });
 
+app.get('/result.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'result.html'));
+});
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`サーバー起動中：http://localhost:${port}`);
 });
